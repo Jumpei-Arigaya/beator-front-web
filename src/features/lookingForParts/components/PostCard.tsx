dc3ef2ff84b617d3dde8ef74ae963fe9ca01d6d0
Jumpei-Arigaya <junpeeei0407@gmail.com>
2023-10-11 22:55:35 +0900

    WIP デザイン修正

--- src/features/lookingForParts/components/PostCard.tsx
+++ src/features/lookingForParts/components/PostCard.tsx
@@ -3,10 +3,11 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Chip from "@mui/material/Chip";
+import { Box } from "@mui/material";
 
 export const PostCard = () => {
   return (
-    <Card sx={{ minWidth: 275, minHeight: 230 }}>
+    <Card sx={{ minWidth: "210px", minHeight: "210px" }}>
       <CardContent>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
           YOSHIKI
@@ -14,11 +15,16 @@ export const PostCard = () => {
         <Typography variant="h5" component="div">
           猫
         </Typography>
-        <Typography variant="body2">募集パート</Typography>
-        <Chip label="ギター" color="secondary" size="small" />
-        <Chip label="ベース" color="secondary" size="small" />
-        <Typography variant="body2">ジャンル</Typography>
-        <Chip label="ロック" color="primary" size="small" />
+        <Box sx={{ marginTop: 1 }}>
+          <Typography variant="body2">募集パート</Typography>
+          <Chip label="ギター" color="secondary" size="small" />
+          <Chip label="ベース" color="secondary" size="small" />
+          <Chip label="サックス" color="secondary" size="small" />
+        </Box>
+        <Box sx={{ marginTop: 1 }}>
+          <Typography variant="body2">ジャンル</Typography>
+          <Chip label="ロック" color="primary" size="small" />
+        </Box>
       </CardContent>
     </Card>
   );
