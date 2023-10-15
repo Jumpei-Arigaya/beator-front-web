36e0d853fd20bfbef1ad78b1ae8bde45937f76ce
Jumpei-Arigaya <junpeeei0407@gmail.com>
2023-10-15 23:43:07 +0900

    コンポーネントのリファクタリング

--- src/features/Profile/components/ProfileIcon.tsx
+++ src/features/Profile/components/ProfileIcon.tsx
@@ -1,6 +1,8 @@
-import * as React from "react";
 import Avatar from "@mui/material/Avatar";
 
+/**
+ * プロフィールに表示するアイコンコンポーネント
+ */
 export const ProfileIcon: React.FC = () => {
   return <Avatar alt="profile-image" src="/face-me.jpg" />;
 };
