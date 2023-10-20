c16c6717a1eb067d9539b8ee877754aa76903266
nonchimaru <non.ari0523@gmail.com>
2023-10-20 20:20:43 +0900

    全ての要素を左右中央揃えにした

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,35 @@
 import { NextPage } from "next";
 import { GuestUserLayout } from "@/components/layouts/GuestUserLayout";
+import { Box, Typography } from "@mui/material";
 
 const Home: NextPage = () => {
   return (
     <main>
-      <GuestUserLayout>エラー画面</GuestUserLayout>
+      <GuestUserLayout>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="h3">404</Typography>
+            <Typography variant="h5">NOT FOUND</Typography>
+          </Box>
+          <Typography variant="body1">
+            お探しのページは見つかりません
+          </Typography>
+        </Box>
+      </GuestUserLayout>
     </main>
   );
 };
