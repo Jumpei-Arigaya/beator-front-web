dadc02a003aff808b95364670be7c4bdfba3b3bf
nonchimaru <non.ari0523@gmail.com>
2023-10-27 15:54:27 +0900

    ユーザーの基本情報の表示のみ

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,13 +1,44 @@
 import { NextPage } from "next";
 import { DashBoard } from "@/components/layouts/DashBoard";
 import { ProfileIcon } from "@/features/Profile/components/ProfileIcon";
+import { Box, Typography } from "@mui/material";
 
 const Profile: NextPage = () => {
   return (
     <main>
       <DashBoard>
-        プロフィール画面
         <ProfileIcon />
+        <Box>
+          <Typography fontSize="h6.fontSize">test0523</Typography>
+          <Typography fontSize="h5.fontSize">のんちまる</Typography>
+        </Box>
+        <Box sx={{ display: "flex", flexDirection: "row" }}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              marginRight: 2,
+            }}
+          >
+            <Typography fontSize="h4.fontSize" sx={{ lineHeight: 0.6 }}>
+              888
+            </Typography>
+            <Typography fontSize="h6.fontSize">follow</Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <Typography fontSize="h4.fontSize" sx={{ lineHeight: 0.6 }}>
+              888
+            </Typography>
+            <Typography fontSize="h6.fontSize">follower</Typography>
+          </Box>
+        </Box>
       </DashBoard>
     </main>
   );
