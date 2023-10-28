56dbd38f349b16d9aa8b4dd15fa5f13161dd67f8
nonchimaru <non.ari0523@gmail.com>
2023-10-28 15:47:05 +0900

    配置、大きさの修正

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -2,42 +2,56 @@ import { NextPage } from "next";
 import { DashBoard } from "@/components/layouts/DashBoard";
 import { ProfileIcon } from "@/features/Profile/components/ProfileIcon";
 import { Box, Typography } from "@mui/material";
+import Divider from "@mui/material/Divider";
 
 const Profile: NextPage = () => {
   return (
     <main>
       <DashBoard>
-        <ProfileIcon width={125} height={125} />
-        <Box>
-          <Typography fontSize="h6.fontSize">test0523</Typography>
-          <Typography fontSize="h5.fontSize">のんちまる</Typography>
-        </Box>
-        <Box sx={{ display: "flex", flexDirection: "row" }}>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              marginRight: 2,
-            }}
-          >
-            <Typography fontSize="h4.fontSize" sx={{ lineHeight: 0.6 }}>
-              888
-            </Typography>
-            <Typography fontSize="h6.fontSize">follow</Typography>
+        <Box sx={{ marginLeft: 30, marginTop: 2 }}>
+          <Box sx={{ display: "flex" }}>
+            <ProfileIcon width={110} height={110} />
+            <Box sx={{ marginLeft: 5, marginTop: 4 }}>
+              <Typography fontSize="h6.fontSize">test0523</Typography>
+              <Typography fontSize="h4.fontSize" sx={{ lineHeight: 1.5 }}>
+                のんちまる
+              </Typography>
+            </Box>
           </Box>
           <Box
             sx={{
               display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
+              flexDirection: "row",
+              marginTop: 2,
             }}
           >
-            <Typography fontSize="h4.fontSize" sx={{ lineHeight: 0.6 }}>
-              888
-            </Typography>
-            <Typography fontSize="h6.fontSize">follower</Typography>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                marginRight: 2,
+              }}
+            >
+              <Typography fontSize="h4.fontSize" sx={{ lineHeight: 0.6 }}>
+                888
+              </Typography>
+              <Typography fontSize="h6.fontSize">follow</Typography>
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Typography fontSize="h4.fontSize" sx={{ lineHeight: 0.6 }}>
+                888
+              </Typography>
+              <Typography fontSize="h6.fontSize">follower</Typography>
+            </Box>
           </Box>
+          <Divider variant="middle" style={{ width: "70%" }} />
         </Box>
       </DashBoard>
     </main>
