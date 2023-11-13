100708cc0838c007b80829d98c7c08b7ce4184c7
nonchimaru <non.ari0523@gmail.com>
2023-11-13 14:22:06 +0900

    プロフィール画面のファイル、ストリーブックの作成

---
+++ src/features/Profile/components/UserProfilePage.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import { DashBoard } from "@/components/layouts/DashBoard";
+import { ProfileIcon } from "@/features/Profile/components/ProfileIcon";
+import { Box, Typography } from "@mui/material";
+
+import { SocialConnection } from "@/features/Profile/components/SocialConnection";
+import { Horizon } from "./Horizon";
+
+/**
+ * プロフィールに表示するプロフィール画面
+ */
+
+type UserProfilePageProps = {};
+
+export const UserProfilePage: React.FC<UserProfilePageProps> = ({}) => {
+  return (
+    <main>
+      <DashBoard>
+        <Box sx={{ marginLeft: 30, marginTop: 2 }}>
+          <Box sx={{ display: "flex" }}>
+            <ProfileIcon width={90} height={90} />
+            <Box sx={{ marginLeft: 5, marginTop: 1 }}>
+              <Typography fontSize="h6.fontSize">test0523</Typography>
+              <Typography fontSize="h4.fontSize" sx={{ lineHeight: 1.5 }}>
+                のんちまる
+              </Typography>
+            </Box>
+          </Box>
+          <SocialConnection />
+          <Horizon />
+        </Box>
+      </DashBoard>
+    </main>
+  );
+};
+
+export default UserProfilePage;
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,30 +1,9 @@
 import { NextPage } from "next";
-import { DashBoard } from "@/components/layouts/DashBoard";
-import { ProfileIcon } from "@/features/Profile/components/ProfileIcon";
-import { Box, Typography } from "@mui/material";
-import Divider from "@mui/material/Divider";
-import { SocialConnection } from "@/features/Profile/components/SocialConnection";
 
-const Profile: NextPage = () => {
-  return (
-    <main>
-      <DashBoard>
-        <Box sx={{ marginLeft: 30, marginTop: 2 }}>
-          <Box sx={{ display: "flex" }}>
-            <ProfileIcon width={90} height={90} />
-            <Box sx={{ marginLeft: 5, marginTop: 1 }}>
-              <Typography fontSize="h6.fontSize">test0523</Typography>
-              <Typography fontSize="h4.fontSize" sx={{ lineHeight: 1.5 }}>
-                のんちまる
-              </Typography>
-            </Box>
-          </Box>
-          <SocialConnection />
-          <Divider variant="middle" style={{ width: "80%" }} />
-        </Box>
-      </DashBoard>
-    </main>
-  );
+import { UserProfilePage } from "@/features/Profile/components/UserProfilePage";
+
+const ProfilePage: NextPage = () => {
+  return <UserProfilePage />;
 };
 
-export default Profile;
+export default ProfilePage;
