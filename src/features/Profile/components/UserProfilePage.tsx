3ce37731e98453aac8ec2da1a73612de3fab6347
nonchimaru <non.ari0523@gmail.com>
2023-11-14 12:51:16 +0900

    プロフィール編集モーダルの追加のみ

--- src/features/Profile/components/UserProfilePage.tsx
+++ src/features/Profile/components/UserProfilePage.tsx
@@ -11,6 +11,8 @@ import { Introduction } from "./Introduction";
 import { CreatedSongs } from "./CreatedSongs";
 
 import { TantouGakki } from "./TantouGakki";
+import { ProfileModal } from "./ProfileIModal";
+import { Settings } from "@mui/icons-material";
 
 /**
  * プロフィールに表示するプロフィール画面
@@ -22,11 +24,22 @@ export const UserProfilePage: React.FC<UserProfilePageProps> = ({}) => {
   return (
     <main>
       <DashBoard>
+        <Box
+          sx={{
+            position: "absolute",
+            left: "80%",
+            transform: "translateX(40%)",
+            marginTop: 3,
+          }}
+        >
+          <ProfileModal />
+        </Box>
         <Box sx={{ marginLeft: 30, marginTop: 2 }}>
           <Box sx={{ display: "flex" }}>
             <ProfileIcon width={90} height={90} />
             <Box sx={{ marginLeft: 5, marginTop: 1 }}>
               <Typography fontSize="h6.fontSize">test0523</Typography>
+
               <Typography fontSize="h4.fontSize" sx={{ lineHeight: 1.5 }}>
                 のんちまる
               </Typography>
