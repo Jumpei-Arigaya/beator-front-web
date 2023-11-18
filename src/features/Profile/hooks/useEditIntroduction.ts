317dc1aa14ca78ed392b4a58e9004df156fe756a
nonchimaru <non.ari0523@gmail.com>
2023-11-18 19:44:01 +0900

    hooksディレクトリ作成

--- src/features/Profile/hooks/useEditIntroduction.ts
+++ src/features/Profile/hooks/useEditIntroduction.ts
@@ -5,6 +5,10 @@ import { useState } from "react";
  */
 export const useEditIntroduction = () => {
   const [textValue, setTextValue] = useState("");
+
+  /**
+   * テキストフィールドの次第文字数をコントロールする
+   */
   const handleTextChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.value.length <= 180) {
       setTextValue(event.target.value);
