2b9362c606aa57ff1b92fc63658168fba3ec6fee
nonchimaru <non.ari0523@gmail.com>
2023-11-21 09:32:27 +0900

    ヘッダ分の上余白の定義指定

--- src/components/layouts/DashBoard.tsx
+++ src/components/layouts/DashBoard.tsx
@@ -1,7 +1,7 @@
 import { Header } from "../elements/Header";
 import Box from "@mui/material/Box";
 import { SoundControlBar } from "@/features/audioPlayer/components/SoundControlBar";
-
+import { HEADER_HEIGHT } from "@/styles/layout";
 type LayoutProps = {
   children: React.ReactNode;
 };
@@ -10,7 +10,14 @@ export const DashBoard: React.FC<LayoutProps> = (props) => {
   return (
     <Box sx={{ display: "flex" }}>
       <Header />
-      <Box sx={{ marginTop: "64px", width: "100%" }}>{props.children}</Box>
+      <Box
+        sx={{
+          marginTop: HEADER_HEIGHT,
+          width: "100%",
+        }}
+      >
+        {props.children}
+      </Box>
       <SoundControlBar />
     </Box>
   );
---
+++ src/styles/layout.ts
@@ -0,0 +1,5 @@
+/**
+ * ヘッダ分の上余白高さ
+ */
+
+export const HEADER_HEIGHT = 8;
